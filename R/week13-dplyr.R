bf6f23daed1d0b0272698b0c40e83776412af08c
Saron M Demeke <demek004@umn.edu>
2023-06-27 14:00:24 -0500

    completed dplyr steps

--- R/week13-dplyr.R
+++ R/week13-dplyr.R
@@ -21,41 +21,54 @@ con <- dbConnect(MariaDB(),
 #workbench so I then used dbGetQuery() to import this table into R and assigned it to week13_tbl then saved it to csv in data folder
 
 
-# week13_tbl <- dbGetQuery(con, "SELECT * FROM cla_tntlab.datascience_8960_table;") %>% 
-#   write_csv("../data/week13.csv")
+dbGetQuery(con, "SELECT * FROM cla_tntlab.datascience_8960_table;") %>%
+  write_csv("../data/week13.csv")
 
 week13_tbl <- read_csv("../data/week13.csv")
 
 
 #Analysis
 
-#First statement just displays number of rows 
+#First statement just displays number of rows and since there are no NAs, displays total number of managers 
 
-total_number_managers <- week13_tbl %>% 
-  nrow() %>%  print()
+week13_tbl %>% 
+  nrow()
 
 #Second statement adds distinct() select only one row per unique employee id. The displayed count
 #is the same as above which means that none of the manager ids were duplicated over more than one row
 
-total_number_uq_managers <- week13_tbl %>% 
+week13_tbl %>% 
   distinct(employee_id) %>% 
-  nrow() %>%  print()
+  nrow()
   
 
 #Third statement chunk filters those not hired originally as mangers based on Y/N manager_hire variable. I then used
 #group_by to split by city location and summarise() to count the length of the employee_id vector in each group
 
-mgr_by_location <- week13_tbl %>% 
+week13_tbl %>% 
   filter(manager_hire == 'N') %>% 
   group_by(city) %>% 
-  summarise(no_mgrs = length(employee_id)) %>% 
-  print()
+  summarise(no_mgrs = n())
+
+#To display the average and standard deviation of number of years of employment, I used group_by() first to split by
+#performance group and then summarise() to calculate both summary statistics within each group
+
+week13_tbl %>% 
+  group_by(performance_group) %>% 
+  summarise(avg_yrs = mean(yrs_employed, na.rm = T),
+            sd_yrs = sd(yrs_employed, na.rm = T))
 
 
-# Display the average and standard deviation of number of years of employment split by performance level (bottom, middle, and top).
 # Display the location and ID numbers of the top 3 managers from each location, in alphabetical order by location and then descending order of test score. If there are ties, include everyone reaching rank 3.
+#To display top 3 scoring (with ties) manager by location, I first grouped the df by location then used arrange() to order
+#each grouped df by alphabetical order of city and descending test score (the .by_group parameter makes sure this ordering
+#is done within each location group). To retain only the top 3 test scores, I used slice_max and retained default with_ties = TRUE so that
+#more than 3 rows are returned if there are ties. Finally, select() retains only the desired variables with all the ordering retained.
 
-
-
+week13_tbl %>% 
+  group_by(city) %>% 
+  arrange(city, desc(test_score), .by_group = T) %>% 
+  slice_max(test_score, n = 3, with_ties = T) %>% 
+  select(city, employee_id) 
 
 
