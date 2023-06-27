100f7652887485ee959deb02eb6156b21cb667d6
Saron M Demeke <demek004@umn.edu>
2023-06-27 12:59:26 -0500

    caught up with SQL statements

--- R/week13-sql.R
+++ R/week13-sql.R
@@ -16,14 +16,27 @@ con <- dbConnect(MariaDB(),
 #Data Import and Cleaning
 
 dbExecute(con, "USE cla_tntlab")
-dbGetQuery(con, "SELECT COUNT(manager_hire) AS total_number_managers FROM datascience_8960_table WHERE manager_hire = 'Y';") 
 
 # 
 # Display the total number of managers.
-##SELECT COUNT(employee_id) AS total_number_managers FROM datascience_8960_table WHERE manager_hire = 'Y';
-# Display the total number of unique managers (i.e., unique by id number).
-##SELECT COUNT(DISTINCT employee_id) AS total_number_uq_managers FROM datascience_8960_table WHERE manager_hire = 'Y';
+dbGetQuery(con," SELECT COUNT(employee_id) AS total_number_managers FROM datascience_8960_table;")
 
-# Display a summary of the number of managers split by location, but only include those who were not originally hired as managers.
-# Display the average and standard deviation of number of years of employment split by performance level (bottom, middle, and top).
-# Display the location and ID numbers of the top 3 managers from each location, in alphabetical order by location and then descending order of test score. If there are ties, include everyone reaching rank 3. You’ll probably need this guideLinks to an external site..
+# Display the total number of unique managers (i.e., unique by id number).
+dbGetQuery(con, "SELECT COUNT(DISTINCT employee_id) AS total_number_uq_managers FROM datascience_8960_table;")
+
+# Display a summary of the number of managers split by location,
+# but only include those who were not originally hired as managers.
+dbGetQuery(con, "SELECT city, COUNT(employee_id) AS no_mgrs FROM datascience_8960_table WHERE manager_hire = 'N' GROUP BY city ;")
+
+# Display the average and standard deviation of number of years of employment 
+  #split by performance level (bottom, middle, and top).
+
+
+# Display the location and ID numbers of the top 3 managers from each location, 
+  #in alphabetical order by location and then descending order of test score. 
+  #If there are ties, include everyone reaching rank 3. You’ll probably need this guideLinks to an external site..
+
+
+
+
+
