a1e9f3114fcbbe0ed796467ea146b0e2dc597ae1
Saron M Demeke <demek004@umn.edu>
2023-06-27 13:52:23 -0500

    cleaned SQL formatting

--- R/week13-sql.R
+++ R/week13-sql.R
@@ -26,12 +26,20 @@ dbGetQuery(con, "SELECT COUNT(DISTINCT employee_id) AS total_number_uq_managers 
 
 # Display a summary of the number of managers split by location,
 # but only include those who were not originally hired as managers.
-dbGetQuery(con, "SELECT city, COUNT(employee_id) AS no_mgrs FROM datascience_8960_table WHERE manager_hire = 'N' GROUP BY city ;")
+dbGetQuery(con, 
+           "SELECT city, COUNT(employee_id) AS no_mgrs 
+           FROM datascience_8960_table 
+           WHERE manager_hire = 'N' 
+           GROUP BY city ;")
+
 
 # Display the average and standard deviation of number of years of employment 
   #split by performance level (bottom, middle, and top).
-dbGetQuery(con, "SELECT performance_group, ROUND(AVG(yrs_employed),2) AS avg_yrs, ROUND(STDDEV_SAMP(yrs_employed),3) AS sd_yrs FROM datascience_8960_table GROUP BY performance_group;
-")
+dbGetQuery(con, 
+           "SELECT performance_group, ROUND(AVG(yrs_employed),2) AS avg_yrs, ROUND(STDDEV_SAMP(yrs_employed),3) AS sd_yrs 
+           FROM datascience_8960_table 
+           GROUP BY performance_group;"
+           )
 
 
 #For this final step, I followed information in provided guide about over clauses and using WITH(). Within the WITH statement,
@@ -42,12 +50,13 @@ dbGetQuery(con, "SELECT performance_group, ROUND(AVG(yrs_employed),2) AS avg_yrs
 #with rank up to 3.
 
 dbGetQuery(con,
-"WITH added_test_rank AS (
-  SELECT	city, employee_id, test_score, RANK() OVER(PARTITION BY city ORDER BY test_score DESC) AS test_rank 
-  FROM datascience_8960_table 
-  ORDER BY city)
-SELECT city, employee_id 
-FROM added_test_rank
-WHERE test_rank <= 3;"
-)
+          "WITH added_test_rank AS (
+           SELECT	city, employee_id, test_score, RANK() OVER(PARTITION BY city ORDER BY test_score DESC) AS test_rank 
+           FROM datascience_8960_table 
+           ORDER BY city)
+          
+           SELECT city, employee_id 
+           FROM added_test_rank
+           WHERE test_rank <= 3;"
+          )
 
